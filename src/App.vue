19eef3e2941848e50221fb395210bcea942fba99
Gabriel <gabriel.bitten59@gmail.com>
2022-08-29 21:05:33 -0300

    Feat(HomePage): pronta

--- src/App.vue
+++ src/App.vue
@@ -6,18 +6,19 @@ import HelloWorld from './components/HelloWorld.vue'
 <template>
   <!-- from base.css => default document font-size: 18px -->
   <!-- Left Side Header -->
-  <header class="h-[100vh] w-[14%] border-4 flex flex-col items-center justify-start">
-    <RouterLink to="/" class="h-auto w-[80%] flex flex-col items-start justify-center border-4 mt-24 text-5xl font-sans font-bold italic">Jodag</RouterLink>
+  <header class="h-[100vh] w-[12%] flex flex-col items-center justify-start ml-4">
+    <!-- Title -->
+    <RouterLink to="/" class="h-auto w-[80%] flex flex-col items-start justify-center  mt-24 text-5xl font-sans font-bold italic">Jodag</RouterLink>
     <!-- Header Nav -->
-    <div class="h-auto w-[80%] flex flex-col items-start justify-center border-4 mt-12">
-      <div class="h-[50px] w-full border-4 mt-1 flex flex-col items-start justify-center"><RouterLink to="/Brand" class="hover:text-[17px] hover:font-[600]">Brand</RouterLink></div>
-      <div class="h-[50px] w-full border-4 mt-1 flex flex-col items-start justify-center"><RouterLink to="/Collection" class="hover:text-[17px] hover:font-[600]">Collection</RouterLink></div>
-      <div class="h-[50px] w-full border-4 mt-1 flex flex-col items-start justify-center"><RouterLink to="/Stores" class="hover:text-[17px] hover:font-[600]">Stores</RouterLink></div>
-      <div class="h-[50px] w-full border-4 mt-1 flex flex-col items-start justify-center"><RouterLink to="/News" class="hover:text-[17px] hover:font-[600]">News</RouterLink></div>
-      <div class="h-[50px] w-full border-4 mt-1 flex flex-col items-start justify-center"><RouterLink to="/Services" class="hover:text-[17px] hover:font-[600]">Services</RouterLink></div>
-      <div class="h-[50px] w-full border-4 mt-1 flex flex-col items-start justify-center"><RouterLink to="/Contact" class="hover:text-[17px] hover:font-[600]">Contact</RouterLink></div>
+    <div class="h-auto w-[80%] flex flex-col items-start justify-center mt-12">
+      <div class="h-[50px] w-full mt-1 flex flex-col items-start justify-center"><RouterLink to="/Marca" class="hover:text-[17px] hover:font-[600]">Marca</RouterLink></div>
+      <div class="h-[50px] w-full mt-1 flex flex-col items-start justify-center"><RouterLink to="/Colecoes" class="hover:text-[17px] hover:font-[600]">Coleções</RouterLink></div>
+      <div class="h-[50px] w-full mt-1 flex flex-col items-start justify-center"><RouterLink to="/Lojas" class="hover:text-[17px] hover:font-[600]">Lojas</RouterLink></div>
+      <div class="h-[50px] w-full mt-1 flex flex-col items-start justify-center"><RouterLink to="/Noticias" class="hover:text-[17px] hover:font-[600]">Notícias</RouterLink></div>
+      <div class="h-[50px] w-full mt-1 flex flex-col items-start justify-center"><RouterLink to="/Servicos" class="hover:text-[17px] hover:font-[600]">Serviços</RouterLink></div>
+      <div class="h-[50px] w-full mt-1 flex flex-col items-start justify-center"><RouterLink to="/Contato" class="hover:text-[17px] hover:font-[600]">Contato</RouterLink></div>
     </div>
-    <div class="h-auto w-[80%] border-4 mt-16 flex flex-col items-start justify-center">
+    <div class="h-auto w-[80%] mt-16 flex flex-col items-start justify-center">
       <a href="https://www.facebook.com" target="_blank" class="h-[auto] w-full flex flex-row items-center justify-start">
         <font-awesome-icon class="text-3xl" icon="fa-brands fa-facebook" />
         <span class="ml-4">Facebook</span>
@@ -30,6 +31,8 @@ import HelloWorld from './components/HelloWorld.vue'
   </header>
 
   <RouterView />
+  <!-- Acessibility e Top Container -->
+  <div class="h-[80px] w-[140px] absolute border-4 bottom-0 right-0"></div>
 </template>
 
 
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -4,8 +4,66 @@
 
 <template>
   <!-- Right Side Content -->
-  <div class="h-[100vh] flex-1 border-4">
-    
+  <div class="h-[auto] flex-1 flex flex-col items-center justify-start mr-14">
+    <!-- Banner -->
+    <div class="h-[600px] w-[90%] mt-24 bg-[url('./assets/Home/Banner3.jpg')] bg-top bg-no-repeat bg-[length:1400px_710px]"></div>
+    <!-- Colecao Title -->
+    <span class="h-[200px] w-[90%] flex flex-col items-center justify-center text-3xl font-['Lato'] font-bold">
+      JODAG COLEÇÃO PRIMAVERA / VERÃO 2022
+    </span>
+    <!-- Main Navigation Container -->
+    <div class="h-[700px] w-[90%] flex flex-col items-center">
+      <!-- Coluna Lojas e Colecoes -->
+      <div class="h-[50%] w-full flex flex-row justify-between">
+        <RouterLink to="Lojas" class="h-full w-[48%] relative flex justify-center items-center group">
+          <img class="h-full w-full" src="../assets/Home/loja1.jpg">
+          <div class="h-[96%] w-[97%] bg-black bg-opacity-40 absolute flex flex-col justify-center items-center">
+            <div class="h-[22%] w-[35%] font-sans font-bold text-4xl text-white text-center tracking-wide">VER LOJAS</div>
+            <div class="h-[13%] w-[23%] mb-12 border-2 border-white text-white flex justify-center items-center font-['Miltonian']">Clique para Ver
+              <div class="h-full w-full bg-black bg-opacity-20 absolute"></div></div>
+          </div>
+          <div class="h-full w-full bg-white absolute bg-opacity-0 group-hover:bg-opacity-10"></div>
+        </RouterLink>
+        <RouterLink to="Colecoes" class="h-full w-[48%] relative flex justify-center items-center group">
+          <img class="h-full w-full" src="../assets/Home/main2.jpg">
+          <div class="h-[96%] w-[97%] bg-black bg-opacity-40 absolute flex flex-col justify-center items-center">
+            <div class="h-[22%] w-[30%] font-sans font-bold text-4xl text-white text-center tracking-wide">COLEÇÕES</div>
+            <div class="h-[13%] w-[23%] mb-12 border-2 border-white text-white flex justify-center items-center font-['Miltonian']">Clique para Ver
+              <div class="h-full w-full bg-black bg-opacity-20 absolute"></div></div>
+          </div>
+          <div class="h-full w-full bg-white absolute bg-opacity-0 group-hover:bg-opacity-10"></div>
+        </RouterLink>
+      </div>
+      <!-- Coluna Marca e Compre Online -->
+      <div class="h-[50%] w-full flex flex-row justify-between mt-10">
+        <RouterLink to="Lojas" class="h-full w-[48%] relative flex justify-center items-center group">
+          <img class="h-full w-full" src="../assets/Home/brand.jpg">
+          <div class="h-[96%] w-[97%] bg-black bg-opacity-40 absolute flex flex-col justify-center items-center">
+            <div class="h-[22%] w-[45%] font-sans font-bold text-4xl text-white text-center tracking-wide">NOSSA MARCA</div>
+            <div class="h-[13%] w-[23%] mb-12 border-2 border-white text-white flex justify-center items-center font-['Miltonian']">Clique para Ver
+              <div class="h-full w-full bg-black bg-opacity-20 absolute"></div></div>
+          </div>
+          <div class="h-full w-full bg-white absolute bg-opacity-0 group-hover:bg-opacity-10"></div>
+        </RouterLink>
+        <RouterLink to="Colecoes" class="h-full w-[48%] relative flex justify-center items-center group">
+          <img class="h-full w-full" src="../assets/Home/buyonline1.jpg">
+          <div class="h-[96%] w-[97%] bg-black bg-opacity-40 absolute flex flex-col justify-center items-center">
+            <div class="h-[22%] w-[50%] font-sans font-bold text-4xl text-white text-center tracking-wide">COMPRE ONLINE</div>
+            <div class="h-[13%] w-[23%] mb-12 border-2 border-white text-white flex justify-center items-center font-['Miltonian']">Clique para Ver
+              <div class="h-full w-full bg-black bg-opacity-20 absolute"></div></div>
+          </div>
+          <div class="h-full w-full bg-white absolute bg-opacity-0 group-hover:bg-opacity-10"></div>
+        </RouterLink>
+      </div>
+    </div>
+    <!-- Footer -->
+    <div class="h-[170px] w-full border-t-2 border-opacity-40 border-[#2c3e50] mt-32 flex flex-col justify-start items-center">
+      <span class="mt-1.5">ⓒ Copyright 2021 Jodag All Rights reserved</span>
+      <span class="mt-1.5">Rua Ipsum Ipsum Ipsum Ipsum, Ipsum / Ipsum</span>
+      <span class="mt-1.5">CNPJ: XX.XXX.XXX/0001-XX</span>
+      <span class="mt-1.5">Central de Atendimento: 0800 000 0000</span>
+      <span class="mt-1.5">Parceria - RH e Marketing: jodagRH@gmail.com</span>
+    </div>
   </div>
 
 </template>
