9ecac517de18dc307777f35a678b94922a590c6f
Gabriel <gabriel.bitten59@gmail.com>
2022-08-29 23:52:10 -0300

    Feat(Geral): Brilho aumentando e diminuindo corretamente, falta tratas erros

--- src/components/Accessibility.vue
+++ src/components/Accessibility.vue
@@ -2,7 +2,8 @@
     export default{
         data(){
             return{
-                IsOpen: false
+                IsOpen: false,
+                Brilho: 0
             }
         },
         methods:{
@@ -25,7 +26,19 @@
                     left: 0,
                     behavior: 'smooth'
                 });
-            }
+            },
+            AumentaBrilho(){
+                var AppDiv = document.getElementById("app")
+                console.log(this.Brilho)
+                this.Brilho += 5
+                AppDiv.style.filter = `grayscale(${(this.Brilho).toString()}%)`
+            },
+            DiminuiBrilho(){
+                var AppDiv = document.getElementById("app")
+                console.log(this.Brilho)
+                this.Brilho -= 5
+                AppDiv.style.filter = `grayscale(${(this.Brilho).toString()}%)`
+            },
         }
     }
 </script>
@@ -33,8 +46,13 @@
 <template>
     <div class="h-[260px] w-[240px] fixed border-4 bottom-0 right-0 flex flex-col items-end justify-end">
         <!-- Footer Dropdown Menu -->
-        <div ref="AccessDropdown" class="h-full w-full bg-black flex flex-col items-center hidden">
-            <button>increase fontsize</button>
+        <div ref="AccessDropdown" class="h-full w-full bg-black bg-opacity-[89%] text-white flex flex-col items-center mb-auto hidden">
+            <span>Tamanho da Fonte</span>
+            <span>Controle de Brilho</span>
+            <div class="h-[40px] w-[70%] flex flex-row justify-center items-center">
+                <font-awesome-icon @click="DiminuiBrilho()" class="m-2 p-1 border-2 border-white cursor-pointer hover:bg-white hover:text-black" icon="fa-solid fa-minus" />
+                <font-awesome-icon @click="AumentaBrilho()" class="m-2 p-1 border-2 border-white cursor-pointer hover:bg-white hover:text-black" icon="fa-solid fa-plus" />         
+            </div>
         </div>
         <div>
             <font-awesome-icon @click="ScrollTop()" class="text-2xl m-1 border-2 border-black p-1 cursor-pointer font-[#2c3e50] hover:bg-black hover:text-white" icon="fa-solid fa-arrows-up-to-line" />
--- src/main.js
+++ src/main.js
@@ -5,8 +5,8 @@ import router from './router'
 import { library } from '@fortawesome/fontawesome-svg-core'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 import { faFacebook,faSquareInstagram } from '@fortawesome/free-brands-svg-icons'
-import { faArrowsUpToLine,faUniversalAccess } from '@fortawesome/free-solid-svg-icons'
-library.add(faFacebook,faSquareInstagram,faArrowsUpToLine,faUniversalAccess)
+import { faArrowsUpToLine,faUniversalAccess,faPlus,faMinus } from '@fortawesome/free-solid-svg-icons'
+library.add(faFacebook,faSquareInstagram,faArrowsUpToLine,faUniversalAccess,faPlus,faMinus)
 
 import './assets/main.css'
 import './tailwind.css'
