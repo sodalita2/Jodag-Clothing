1b12e1361379b7a8867d09f086039244ed2975f2
Gabriel <gabriel.bitten59@gmail.com>
2022-09-02 08:42:17 -0300

    Fix(HeaderLi Component): fixei o RouterLink do Header nav

--- src/components/HeaderLI.vue
+++ src/components/HeaderLI.vue
@@ -12,5 +12,5 @@ defineProps({
 </script>
 
 <template>
-  <div class="h-[50px] w-full mt-1 flex flex-col items-start justify-center"><RouterLink to="/{{ destination }}" class="hover:text-[17px] hover:font-[600]">{{ content }}</RouterLink></div>
+  <div class="h-[50px] w-full mt-1 flex flex-col items-start justify-center"><RouterLink :to="destination" class="hover:text-[17px] hover:font-[600]">{{ content }}</RouterLink></div>
 </template>
